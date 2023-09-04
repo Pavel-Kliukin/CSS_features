79bfef3f01663156bb29dd6512ce14a6dfa2fa36
Pavel-Kliukin <9213087@mail.ru>
2023-09-05 01:47:03 +0300

    Fancy boxes with shadows added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='big_box'>
+        <h2>Some intresting box</h2>
+        <div className='smaller_box'>
+          <div className='square'>
+            1
+          </div>
+          <div className='square'>
+            2
+          </div>
+          <div className='square'>
+            3
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
